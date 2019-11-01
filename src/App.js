31d700410e5222f3a88fd35acaf6c765c0b63892
Zoee Silcock <mrzoee@gmail.com>
2019-11-01 16:05:00 +0100

    Add basic brown noise with a toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import Tone from 'tone';
 import './App.css';
+
+let noise = null;
+
+function initNoise() {
+  noise = new Tone.Noise("brown").toMaster();
+  noise._playbackRate = 0.1;
+}
+
+function toggleNoise() {
+  if (noise == null) {
+    initNoise();
+  }
+
+  if (noise.state === 'stopped') {
+    noise.start();
+  } else {
+    noise.stop();
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Quick Noise</h1>
+        <button onClick={() => toggleNoise()}>Toggle noise</button>
       </header>
     </div>
   );
