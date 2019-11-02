9af268a88bc8de29d8267ea6465cd2f3d7812883
Zoee Silcock <mrzoee@gmail.com>
2019-11-02 11:51:33 +0100

    Use socket.io to allow remote controlling the player from any device

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
@@ -11,6 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // API routes.
 app.get('/api/hello', (req, res) => {
   res.send({ response: 'Hello from Quick Noise API' });
+});
+
+// Socket.
+io.on('connection', (socket) => {
+  socket.on('toggle noise', () => {
+    io.emit('toggle noise');
+  });
 });
 
 // Serve the frontend in production.
@@ -22,4 +31,4 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+http.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,35 +1,66 @@
 import React from 'react';
 import Tone from 'tone';
+import io from 'socket.io-client';
+
 import './App.css';
+import RemoteToggle from './components/RemoteToggle';
 
-let noise = null;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function initNoise() {
-  noise = new Tone.Noise("brown").toMaster();
-  noise._playbackRate = 0.1;
-}
+    this.state = { isRemote: true };
+    this.noise = null;
 
-function toggleNoise() {
-  if (noise == null) {
-    initNoise();
+    this.socket = io(`${window.location.hostname}:5000`);
+    this.socket.on('toggle noise', () => {
+      if (!this.state.isRemote) {
+        this.togglePlayer();
+      }
+    });
   }
 
-  if (noise.state === 'stopped') {
-    noise.start();
-  } else {
-    noise.stop();
+  initNoise() {
+    this.noise = new Tone.Noise("brown").toMaster();
+    this.noise._playbackRate = 0.1;
   }
-}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Quick Noise</h1>
-        <button className="App-button" onClick={() => toggleNoise()}>Toggle noise</button>
-      </header>
-    </div>
-  );
+  toggleNoise() {
+    if (this.state.isRemote) {
+      this.socket.emit('toggle noise');
+    } else {
+      this.togglePlayer();
+    }
+  }
+
+  togglePlayer() {
+    if (this.noise == null) {
+      this.initNoise();
+    }
+
+    if (this.noise.state === 'stopped') {
+      this.noise.start();
+    } else {
+      this.noise.stop();
+    }
+  }
+
+  toggleRemote() {
+    this.setState({ isRemote: !this.state.isRemote });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Quick Noise</h1>
+          <button className="App-button" onClick={() => this.toggleNoise()}>Toggle noise</button>
+          <br/>
+          <RemoteToggle isRemote={this.state.isRemote} onClick={() => this.toggleRemote()} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
