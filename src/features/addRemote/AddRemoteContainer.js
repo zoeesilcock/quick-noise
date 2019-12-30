a6f9a82a939b13733c56c05eb87a8063852299a0
Zoee Silcock <mrzoee@gmail.com>
2019-12-30 11:56:10 +0100

    Only show the addRemote feature when in player mode

--- src/features/addRemote/AddRemoteContainer.js
+++ src/features/addRemote/AddRemoteContainer.js
@@ -4,30 +4,38 @@ import { connect } from 'react-redux';
 import { addRemote, hideCode } from './addRemoteSlice';
 import AddRemoteButton from './AddRemoteButton';
 import AddRemoteCode from './AddRemoteCode';
+import { AppModes } from '../appMode/appModeSlice';
 
 const mapStateToProps = state => ({
   isFetching: state.addRemote.fetchingCode,
   newRemoteCode: state.addRemote.newRemoteCode,
   isShowingCode: state.addRemote.showingCode,
+  appMode: state.appMode,
 });
 
 const mapDispatchToProps = { addRemote, hideCode };
 
 class AddRemoteContainer extends React.Component {
   render() {
-    return (
-      <div>
-        <AddRemoteButton
-          isFetching={this.props.isFetching}
-          addRemote={this.props.addRemote}
-        />
-        <AddRemoteCode
-          newRemoteCode={this.props.newRemoteCode}
-          isShowingCode={this.props.isShowingCode}
-          hideCode={this.props.hideCode}
-        />
-      </div>
-    );
+    let jsx = null;
+
+    if (this.props.appMode === AppModes.PLAYER) {
+      jsx = (
+        <div>
+          <AddRemoteButton
+            isFetching={this.props.isFetching}
+            addRemote={this.props.addRemote}
+          />
+          <AddRemoteCode
+            newRemoteCode={this.props.newRemoteCode}
+            isShowingCode={this.props.isShowingCode}
+            hideCode={this.props.hideCode}
+          />
+        </div>
+      );
+    }
+
+    return jsx;
   }
 }
 
